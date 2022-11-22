de3f20b5e0617c6b142973687f08ce19e9216cba
Anwar Sarmiento <anwarsarmiento@gmail.com>
2022-11-22 10:30:31 -0600

    fix: cambiando ruta principal

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('chat');
 });
 Route::get('chat', [ChatController::class,'chat'])->name('chat');
 Route::post('send', [ChatController::class,'send']);
